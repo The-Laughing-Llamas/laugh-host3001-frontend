b4223863fd63e14952a14bac8a5405ed6a6a6935
Sean <swbriski@gmail.com>
2024-01-11 15:17:56 -0800

    added signin and signup functionality and testing

---
+++ src/__tests__/SignUp.test.js
@@ -0,0 +1,56 @@
+import { render, screen } from "@testing-library/react";
+import SignUp from "../pages/SignUp";
+import { BrowserRouter } from "react-router-dom";
+
+describe("<SignUp />", () => {
+  const signup = () => {
+    render(
+      <BrowserRouter>
+        <SignUp />
+      </BrowserRouter>
+    );
+  };
+
+  it("renders a without errors", () => {
+    signup();
+  });
+
+  it("has a heading", () => {
+    signup();
+    expect(
+      screen.getByRole("heading", {
+        name: /Sign Up/i,
+      })
+    ).toBeInTheDocument();
+    screen.logTestingPlaygroundURL()
+  });
+
+  it("has an email and password label", () => {
+    signup();
+    expect(screen.getByText(/email/i)).toBeInTheDocument();
+    expect(screen.getByText("Password")).toBeInTheDocument();
+    expect(screen.getByText(/confirm password/i)).toBeInTheDocument();
+  });
+
+  it("has a textbox for email and password", () => {
+    signup();
+    expect(
+        screen.getByRole("textbox", {
+          name: /email/i,
+        })
+      ).toBeInTheDocument();
+      expect(
+        screen.getByLabelText(/password/i)
+      ).toBeInTheDocument();
+      expect(
+        screen.getByPlaceholderText(/confirm pw/i)
+      ).toBeInTheDocument();
+  });
+
+  it("has a login button", () => {
+    signup();
+    expect(screen.getByRole('button', {
+        name: /signup/i
+      })).toBeInTheDocument();
+  });
+});
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,8 +1,68 @@
 import React from 'react'
+import { useRef } from 'react'
+import { Form, FormGroup, Input, Label, Button } from 'reactstrap'
+import { useNavigate } from 'react-router-dom'
 
-const SignUp = () => {
+const SignUp = ( {signup} ) => {
+
+  const formRef = useRef()
+
+  const navigate = useNavigate()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const formData = new FormData(formRef.current)
+    const data = Object.fromEntries(formData)
+    const userInfo = {
+      user: { email: data.email, password: data.password }
+    }
+    signup(userInfo)
+    navigate("/login")
+    e.target.reset()
+  }
+
   return (
-    <div>SignUp</div>
+    <>
+      <div><h1>Sign Up</h1></div>
+      <form ref={formRef} onSubmit={handleSubmit}>
+        <FormGroup floating>
+          <Input
+            id="exampleEmail"
+            name="email"
+            placeholder="Email"
+            type="email"
+          />
+          <Label for="exampleEmail">
+            Email
+          </Label>
+        </FormGroup>
+        {' '}
+        <FormGroup floating>
+          <Input
+            id="examplePassword"
+            name="password"
+            placeholder="Password"
+            type="password"
+          />
+          <Label for="examplePassword">
+            Password
+          </Label>
+        </FormGroup>
+        <FormGroup floating>
+          <Input
+            id="confirmPW"
+            name="pw_confirmation"
+            placeholder="Confirm PW"
+            type="password"
+          />
+          <Label for="confirmPassword">
+            Confirm Password
+          </Label>
+        </FormGroup>
+        {' '}
+        <Input type="submit" value="Signup" />
+      </form>
+      </>
   )
 }
 
